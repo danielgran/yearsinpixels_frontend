89b959fdd6e0e24b02c6a1aa468d796212921c30
Daniel Gran <daniel.gran@mailbox.org>
2021-06-29 21:36:41 +0200

    Inital Commit

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="container">
+    <div id="header">
+      <div class="header-item">
+        <i class="fab -user-circle" />
+      </div>
+    </div>
+    ffas
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Dashboard",
+  computed: {
+    username() {
+      return this.$store.getters.getName
+    }
+  }
+}
+</script>
+
+<style scoped>
+#container {
+  background: #ECECEC;
+  width: 100vw;
+  min-height: 100vh;
+}
+
+#header {
+  width: 100vw;
+  height: 40px;
+  background: gray;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header-item {
+  margin: 0 10px;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,63 @@
+import 'es6-promise/auto'
+
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
+import Dashboard from "@/components/Dashboard";
+import Login from "@/components/Login";
+
+// UI and stuff
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+// Connect the "addons"
+Vue.use(Vuex)
+Vue.use(VueRouter)
+
+
+// Initialize Vuex Store
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    name: "Daniel Gran"
+  },
+  getters: {
+    getName() {
+      return "Daniel Gran"
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+// Initialize Vue-Router
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Dashboard
+    },
+    {
+      component: Login,
+      name: 'Login',
+      path: '/login'
+    }
+  ],
+  mode: 'history'
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router: router,
+  store: store,
+  render: h => h(App)
+})
