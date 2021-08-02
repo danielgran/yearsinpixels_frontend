1420c3849a3a2d2b5138f2796583e7f5adf540d8
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-08-02 20:26:13 +0200

    Made Router more Usable and updated naming

---
+++ src/vue/Statemanagement/Configuration/DefaultState.ts
@@ -0,0 +1,18 @@
+
+import Day from "@/Model/Day";
+import User from "@/Model/User";
+import IState from "@/vue/Statemanagement/IState";
+
+export default class DefaultState implements IState {
+  name: String;
+  days: Day[];
+  localUser: User;
+
+  constructor() {
+    this.name = "Testing key"
+    this.days = []
+    this.days.push(new Day());
+
+    this.localUser = new User();
+  }
+}
---
+++ src/vue/Statemanagement/VuexStore.ts
@@ -0,0 +1,29 @@
+import { createStore, Store } from "vuex";
+import createPersistedState from "vuex-persistedstate"
+
+
+import IState from "./IState";
+import IVueStorePlugin from "@/vue/Statemanagement/IVueStorePlugin";
+import IMutations from "./IMutations";
+import IActions from "./IActions";
+
+
+export class VuexStore implements IVueStorePlugin {
+
+  plugin: any;
+  store: Store<any> | undefined;
+
+  constructor(state: IState, mutations: IMutations, actions: IActions) {
+
+    let store = createStore({
+      state: state,
+      mutations: mutations.Mutations,
+      actions: actions.Actions,
+      plugins: [
+        createPersistedState(),
+      ]
+    })
+
+    this.plugin = store
+  }
+}
