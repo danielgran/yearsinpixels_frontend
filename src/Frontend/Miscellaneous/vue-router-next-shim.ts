b4aff9e878d9e5eb11275bd0efbcf0387afcc55e
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-12-15 06:17:57 +0100

    Preperation

--- src/Frontend/Miscellaneous/vue-router-next-shim.ts
+++ src/Frontend/Miscellaneous/vue-router-next-shim.ts
@@ -1,8 +1,8 @@
-import { DefaultVuexStore } from "@/Frontend/Statemanagement/VuexStore";
 import { Store } from "vuex";
-import IState from "./IState";
+import IState from "@/Frontend/Statemanagement/IState";
 
 declare module "@vue/runtime-core" {
+  // eslint-disable-next-line no-unused-vars
   interface ComponentCustomProperties {
     $store: Store<IState>;
   }
