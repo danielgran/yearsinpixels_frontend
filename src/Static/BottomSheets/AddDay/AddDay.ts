b135d74d948a30586e06dd988a15bf44274adeff
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-12-20 20:29:55 +0100

    Added: Bound remaining values

--- src/Static/BottomSheets/AddDay/AddDay.ts
+++ src/Static/BottomSheets/AddDay/AddDay.ts
@@ -3,19 +3,42 @@ import {mapState} from "vuex";
 
 export default defineComponent({
   name: "AddDay",
-  computed: mapState([
-    'show_add_day_in_dashboard',
-    'moods'
-  ]),
+  computed: {
+    ...mapState([
+               'show_add_day_in_dashboard',
+               'moods'
+             ]),
+    send_button_color: function () {
+      if (this.selected_mood_object == null)
+        return 'background: grey;';
+      return "background: #3c79fa";
+
+    },
+    select_box_background_color: function() {
+      if (this.selected_mood_object == null)
+        return 'background-color: rgb(221, 221, 221);';
+      // @ts-ignore
+      let color = this.selected_mood_object.color;
+      color = color.toString(16);
+
+      return 'background-color: #' + color + ';';
+    }
+  },
   data: function () {
     return {
       box_title: "",
       box_notes: "",
-      selected_mood_id: 0
+      selected_mood_object:  null,
+      send_button_enabled: false
     };
   },
   methods: {
     send: function() {
+      if (this.selected_mood_object == null)
+        return;
+
+    },
+    close: function () {
       this.$store.commit("SetShowDialogInDashboard", false);
     }
   }
--- src/Static/BottomSheets/AddDay/AddDay.vue
+++ src/Static/BottomSheets/AddDay/AddDay.vue
@@ -3,16 +3,22 @@
     <div>
       <div id="addDay">
         <div id="content">
+        <div id="close" v-on:click="close">
+          <span>
+            <i class="editIcon fas fa-times"/>
+          </span>
+        </div>
           <h2>Your day was...</h2>
           <form>
             <input v-model="box_title" type="text" placeholder="Title"/>
             <textarea v-model="box_notes" placeholder="Notes"/>
-            <select v-model="selected_mood_id">
-              <option v-for="mood in moods" :key="mood.id" :value="mood.id">
+            <select v-model="selected_mood_object" :style="select_box_background_color">
+              <option value="" disabled selected>Auswählen</option>
+              <option v-for="mood in moods" :key="mood.id" :value="mood">
                 {{ mood.title }}
               </option>
             </select>
-            <input type="button" value="Abschicken" @click="send"/>
+            <input type="button" value="Abschicken" @click="send" :style="send_button_color"/>
           </form>
         </div>
       </div>
