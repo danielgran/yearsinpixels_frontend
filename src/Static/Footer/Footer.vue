af2af0a9aa6b9d84c7043d310b83c2db0f2964b5
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2022-01-08 11:21:07 +0100

    Updated: Styling

--- src/Static/Footer/Footer.vue
+++ src/Static/Footer/Footer.vue
@@ -2,16 +2,22 @@
   <div>
     <div id="footer">
       <div id="content">
-        <div class="footer-section">
-          <h4>Rechtliches</h4>
-          <ul class="right-1">
-            <li><router-link to="/imprint">Impressum</router-link></li>
-            <li><router-link to="/privacypolicy">Datenschutzerklärung</router-link></li>
-          </ul>
+        <div id="footer-parts">
+          <div class="footer-section">
+            <h4>Rechtliches</h4>
+            <ul class="right-1">
+              <li>
+                <router-link to="/imprint">Impressum</router-link>
+              </li>
+              <li>
+                <router-link to="/privacypolicy">Datenschutzerklärung</router-link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div id="footer-footer">
-        <p>© 2021 - {{ date_today.getFullYear() }} Daniel Gran</p>
+        <div id="footer-footer">
+          <p>© 2021 - {{ date_today.getFullYear() }} Daniel Gran</p>
+        </div>
       </div>
     </div>
   </div>
