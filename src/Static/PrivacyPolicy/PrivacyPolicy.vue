0206971fddd7280ad97ce2d864736f0281bd246a
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2022-01-08 10:32:05 +0100

    Added: Styling

--- src/Static/PrivacyPolicy/PrivacyPolicy.vue
+++ src/Static/PrivacyPolicy/PrivacyPolicy.vue
@@ -3,26 +3,26 @@
     <div id="privacypolicy">
       <h1>Datenschutzerklärung</h1>
       <p class="black right-1 italic">Hier finden Sie wichtige Angaben zur Verarbeitung der auf dieser Website von Ihnen verarbeiteten Daten.</p>
+
       <div>
         <h2 class="right-1">Einleitung</h2>
         <p class="black italic right-1">(Standardtext)</p>
         <p class="black right-2 moretext">Die Nutzung dieser Website kann mit der Verarbeitung von personenbezogenen Daten verbunden sein. Damit diese Verarbeitungen für Sie nachvollziehbar sind, möchten wir Ihnen mit den folgenden Informationen einen Überblick zu diesen Verarbeitungen verschaffen.</p>
       </div>
+
       <div>
         <h2 class="right-1">Kontaktdaten zur Person in Datenschutzsachen</h2>
         <p class="black right-2">Finden Sie <router-link to="/imprint">hier</router-link>.</p>
       </div>
+
       <div>
         <h2 class="right-1">Allgemeine Angaben zur Datenschutzverarbeitung</h2>
         <p class="black right-2 moretext">Wir verarbeiten personenbezogene Daten unter Beachtung der einschlägigen Datenschutzvorschriften, insbesondere der DSGVO und des BDSG. Eine Datenverarbeitung durch uns findet nur auf der Grundlage einer gesetzlichen Erlaubnis statt. Bei der Nutzung dieser Website verarbeiten wir personenbezogene Daten nur mit Ihrer Einwilligung (Art. 6 Abs. 1 Buchst. a) DSGVO), zur Erfüllung eines Vertrags, dessen Vertragspartei Sie sind, oder auf Ihre Anfrage zur Durchführung vorvertraglicher Maßnahmen (Art. 6 Abs. 1 Buchst. b) DSGVO), zur Erfüllung einer rechtlichen Verpflichtung (Art. 6 Abs. 1 Buchst. c) DSGVO) oder wenn die Verarbeitung zu Wahrung unser berechtigten Interessen oder den berechtigten Interessen eines Dritten erforderlich ist, sofern nicht Ihre Interessen oder Grundrechte und Grundfreiheiten, die den Schutz personenbezogener Daten erfordern, überwiegen (Art. 6 Abs. 1 Buchst. f) DSGVO).</p>
       </div>
+
       <div>
         <h2 class="right-1">Dauer der Speicherung</h2>
         <p class="black right-2 moretext">Die angegebenen Daten werden nicht automatisch gelöscht. Insofern Sie dies wünschen, kontaktieren Sie uns zwecks einer Datensatzlöschung.</p>
-      </div>
-      <div>
-        <h2 class="right-1">Dauer der Speicherung</h2>
-        <p class="black right-2 moretext"></p>
       </div>
 
       <div>
@@ -39,7 +39,6 @@
         <h2 class="right-1">Cookies</h2>
         <p class="black right-2 moretext">Wir verwenden auf unserer Website Cookies. Bei Cookies handelt es sich um kleine Textdateien, die durch Ihren Browser gespeichert werden, wenn Sie eine Website besuchen. Hierdurch wird der verwendete Browser gekennzeichnet und kann durch unseren Webserver wiedererkannt werden. Dabei verwenden wir insbesondere dauerhafte Cookies („Persistent Cookies“) für die Analyse unserer Website. Diese Cookies werden automatisiert nach einer vorgegebenen Dauer gelöscht, die sich je nach Cookie unterscheiden kann. Sofern es durch diese Verwendung von Cookies zu einer Verarbeitung personenbezogener Daten kommt, beruht diese auf der Rechtsgrundlage des Art. 6 Abs. 1 Buchst. f) DSGVO. Diese Verarbeitung dient unserem berechtigten Interesse, unsere Website nutzerfreundlicher, effektiver sowie sicherer zu machen. Sie können die Cookies in den Sicherheitseinstellungen Ihres Browsers jederzeit löschen. Sie können der Verwendung von Cookies durch Ihre Browsereinstellungen grundsätzlich widersprechen.</p>
       </div>
-
 
     </div>
   </div>
